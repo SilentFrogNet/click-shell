fed26a77adf8d0cd67b1fb1e0d5118b259b73c6e
SilentFrogNet <info@silentfrog.net>
2018-10-29 12:23:12 +0100

    Commented out not working decorator

--- click_shell/__init__.py
+++ click_shell/__init__.py
@@ -5,7 +5,7 @@ An extension to click that easily turns your click app into a shell utility
 """
 
 from .core import make_click_shell, Shell, add_shell_only_command
-from .decorators import shell, shell_only
+from .decorators import shell   #, shell_only
 
 
 __all__ = [
@@ -13,5 +13,5 @@ __all__ = [
     'shell',
     'Shell',
     'add_shell_only_command',
-    'shell_only',
+    # 'shell_only',
 ]
--- click_shell/decorators.py
+++ click_shell/decorators.py
@@ -6,7 +6,7 @@ Decorators to make using click_shell simpler and more similar to click.
 
 import click
 
-from .core import Shell
+from .core import Shell, add_shell_only_command
 
 
 def shell(name=None, **attrs):
@@ -16,3 +16,14 @@ def shell(name=None, **attrs):
     """
     attrs.setdefault('cls', Shell)
     return click.command(name, **attrs)
+
+
+# def shell_only(cmd=None, **attrs):
+#     # TODO: not working! :(
+#     shell = attrs.get('shell', None)
+#     name = attrs.get('name', None)
+#     def decorator(f):
+#         if shell:
+#             add_shell_only_command(shell, f, name=name)
+#         return cmd
+#     return decorator
